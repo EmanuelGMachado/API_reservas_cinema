3930f56b8a6e296f4ccaef0ccee3b6003a594bd3
Emanuel Gomes Machado <emanueltbg@hotmail.com>
2026-02-08 12:13:23 -0300

    Banco de sessões
    Adiciona a estrutura inicial dos dados para as sessões do cinema

---
+++ app/banco.py
@@ -0,0 +1,92 @@
+[
+  {
+    "id_sessao": 1,
+    "titulo_filme": "O senhor dos anéis: A sociedade do anel",
+    "sala": "Sala 1",
+    "horario": "19:00",
+    "assentos": [
+      { "id": "A-01", "disponivel": true },
+      { "id": "A-02", "disponivel": true },
+      { "id": "A-03", "disponivel": true },
+      { "id": "A-04", "disponivel": true },
+      { "id": "A-05", "disponivel": true },
+      { "id": "A-06", "disponivel": true },
+      { "id": "A-07", "disponivel": true },
+      { "id": "A-08", "disponivel": true },
+      { "id": "A-09", "disponivel": true },
+      { "id": "A-10", "disponivel": true }
+    ]
+  },
+  {
+    "id_sessao": 2,
+    "titulo_filme": "Harry Potter e a pedra filosofal",
+    "sala": "Sala 2",
+    "horario": "20:00",
+    "assentos": [
+      { "id": "B-01", "disponivel": true },
+      { "id": "B-02", "disponivel": true },
+      { "id": "B-03", "disponivel": true },
+      { "id": "B-04", "disponivel": true },
+      { "id": "B-05", "disponivel": true },
+      { "id": "B-06", "disponivel": true },
+      { "id": "B-07", "disponivel": true },
+      { "id": "B-08", "disponivel": true },
+      { "id": "B-09", "disponivel": true },
+      { "id": "B-10", "disponivel": true }
+    ]
+  },
+  {
+    "id_sessao": 3,
+    "titulo_filme": "Monstros S.A.",
+    "sala": "Sala 3",
+    "horario": "18:30",
+    "assentos": [
+      { "id": "C-01", "disponivel": true },
+      { "id": "C-02", "disponivel": true },
+      { "id": "C-03", "disponivel": true },
+      { "id": "C-04", "disponivel": true },
+      { "id": "C-05", "disponivel": true },
+      { "id": "C-06", "disponivel": true },
+      { "id": "C-07", "disponivel": true },
+      { "id": "C-08", "disponivel": true },
+      { "id": "C-09", "disponivel": true },
+      { "id": "C-10", "disponivel": true }
+    ]
+  },
+  {
+    "id_sessao": 4,
+    "titulo_filme": "Onze homens e um segredo",
+    "sala": "Sala 4",
+    "horario": "21:00",
+    "assentos": [
+      { "id": "D-01", "disponivel": true },
+      { "id": "D-02", "disponivel": true },
+      { "id": "D-03", "disponivel": true },
+      { "id": "D-04", "disponivel": true },
+      { "id": "D-05", "disponivel": true },
+      { "id": "D-06", "disponivel": true },
+      { "id": "D-07", "disponivel": true },
+      { "id": "D-08", "disponivel": true },
+      { "id": "D-09", "disponivel": true },
+      { "id": "D-10", "disponivel": true }
+    ]
+  },
+  {
+    "id_sessao": 5,
+    "titulo_filme": "Vanilla Sky",
+    "sala": "Sala 5",
+    "horario": "22:00",
+    "assentos": [
+      { "id": "E-01", "disponivel": true },
+      { "id": "E-02", "disponivel": true },
+      { "id": "E-03", "disponivel": true },
+      { "id": "E-04", "disponivel": true },
+      { "id": "E-05", "disponivel": true },
+      { "id": "E-06", "disponivel": true },
+      { "id": "E-07", "disponivel": true },
+      { "id": "E-08", "disponivel": true },
+      { "id": "E-09", "disponivel": true },
+      { "id": "E-10", "disponivel": true }
+    ]
+  }
+]
